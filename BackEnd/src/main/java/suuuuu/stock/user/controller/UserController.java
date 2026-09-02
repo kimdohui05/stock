9981d731296d2dbdf007bb2044cc90d8c5fc34a8
rlaehgml0830 <kimdohui05@gmail.com>
2026-09-02 10:32:34 +0900

    로그인 완성

--- BackEnd/src/main/java/suuuuu/stock/user/controller/UserController.java
+++ BackEnd/src/main/java/suuuuu/stock/user/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import suuuuu.stock.user.dto.UserLoginRequestDto;
 import suuuuu.stock.user.dto.UserRegisterRequestDto;
 import suuuuu.stock.user.service.UserService;
 
@@ -18,6 +19,13 @@ public class UserController {
     @PostMapping("/register")
     public void register (@RequestBody UserRegisterRequestDto userRegisterRequestDto) {
         userService.register(userRegisterRequestDto);
+    }
+
+    @PostMapping("/login")
+    public String login (@RequestBody UserLoginRequestDto userLoginRequestDto) {
+        userService.login(userLoginRequestDto);
+
+        return userService.login(userLoginRequestDto);
     }
 }
 
